2d9ab4fd698aafc1cb673427393e85e0ed952b32
Guilherme Augusto da Rocha Manhani <gmanhani@sup05.unicred-parana.corp>
2021-09-30 17:39:06 -0300

    configurando sqlite

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -7,7 +7,7 @@ class SplashPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(''),
+        title: const Text('To do list'),
       ),
       body: Container(),
     );
