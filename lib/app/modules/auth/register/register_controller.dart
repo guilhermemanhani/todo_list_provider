192298df4b87558d27b4602d01aee2ec5b505adf
Guilherme Augusto da Rocha Manhani <gmanhani@sup05.unicred-parana.corp>
2021-10-06 14:48:24 -0300

    registrando usuario

--- lib/app/modules/auth/register/register_controller.dart
+++ lib/app/modules/auth/register/register_controller.dart
@@ -1,3 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todolist_provider/app/exception/auth_exceptions.dart';
+import 'package:flutter_todolist_provider/app/services/user/user_service.dart';
 
-class RegisterController extends ChangeNotifier {}
+class RegisterController extends ChangeNotifier {
+  final UserService _userService;
+  String? error;
+  bool success = false;
+
+  RegisterController({required UserService userService})
+      : _userService = userService;
+
+  Future<void> registerUser(String email, String password) async {
+    try {
+      error = null;
+      success = false;
+      notifyListeners();
+      final user = await _userService.register(email, password);
+      if (user != null) {
+        // sucesso
+        success = true;
+      } else {
+        // erro
+        error = 'Erro ao registrar usuario';
+      }
+    } on AuthExceptions catch (e) {
+      error = e.message;
+    } finally {
+      notifyListeners();
+    }
+  }
+}
